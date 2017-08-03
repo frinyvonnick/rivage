80e2969b23629b09ce8cf686092470a3ddb340a7
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-08-03 15:42:35 +0200

    take care of @nlepage review

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -4,20 +4,20 @@ const shallowEqualObjects = require('shallow-equal/objects')
 const { getStore } = require('state/store')
 
 // eslint-disable-next-line no-empty-function
-module.exports = (mapStateToProps = noop, mapDispatchToProps = noop) => Component => {
+module.exports = (mapStateToData = noop, mapDispatchToData = noop) => Component => {
   let instance
   let data
   let options
   const store = getStore()
 
   const getNextData = (state, data, dispatch) => {
-    const stateToProps = mapStateToProps(state, data)
-    const dispatchToProps = mapDispatchToProps(dispatch, data)
+    const stateToData = mapStateToData(state, data)
+    const dispatchToData = mapDispatchToData(dispatch, data)
 
-    return Object.assign({}, data, stateToProps, dispatchToProps)
+    return Object.assign({}, data, stateToData, dispatchToData)
   }
 
-  store.subscribe(() => {
+  const unsubscribe = store.subscribe(() => {
     const nextData = getNextData(store.getState(), options.data, store.dispatch)
 
     if (!shallowEqualObjects(data, nextData)) {
@@ -33,6 +33,10 @@ module.exports = (mapStateToProps = noop, mapDispatchToProps = noop) => Componen
     const newOptions = Object.assign({}, options, { data })
 
     instance = new Component(newOptions)
+
+    instance.on('destroy', () => {
+      unsubscribe()
+    })
 
     return instance
   }
